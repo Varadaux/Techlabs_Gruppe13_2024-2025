0cf85f6421e410c88c8b48ae8c3f951c742f930a
Varadaux <patric.s.keister@gmail.com>
2025-03-03 14:46:10 +0100

    Add files via upload

---
+++ Streamlit_App.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install streamlit
+
+
+# In[2]:
+
+
+import streamlit as st
+import folium
+from streamlit.components.v1 import iframe
+
+
+# In[3]:
+
+
+st.title("Displaying Münster’s socioeconomic and educational situation geographically")
+
+
+# In[4]:
+
+
+st.header("Münster's Socioeconomic Situation ")
+
+with open("SchulDaten_Heatmap.html", 'r') as file:
+    st.components.v1.html(file.read(), height=600)
+
+
+# In[5]:
+
+
+st.header("Münster's Educational Situation ")
+
+
+# In[6]:
+
+
+with open("SchulDaten_Heatmap.html", 'r') as file:
+    st.components.v1.html(file.read(), height=600)
+
